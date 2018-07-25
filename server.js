b3d24ff5718c16f522d49e7ff000b769d95accbf
Matthew Croak <matthewcroak@Matthews-MBP.home>
2018-07-25 13:14:16 -0400

    missing closing parenthesis on line 5 in server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('build'));
-app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html'));
+app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')));
 
 app.listen(process.env.PORT, () => console.log('listening for connections'));
